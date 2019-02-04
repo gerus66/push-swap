6523975c7a0cc8acb8731f17826cd6f55aba0f9f
Merlon crakehall Bartoletti <mbartole@e2r10p5.21-school.ru>
2019-02-04 15:07:15 +0300

    put all operations with stack to my_stack.c

--- my_stack.c
+++ my_stack.c
@@ -6,11 +6,14 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/26 18:51:58 by mbartole          #+#    #+#             */
-/*   Updated: 2019/01/27 01:10:16 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/02/04 10:27:32 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "checker.h"
+#include "swap.h"
+
+#define I_MAX 2147483647
+#define I_MIN -2147483648
 
 void	swap_stack(t_list **st)
 {
@@ -63,4 +66,44 @@ void	r_rotate_stack(t_list **st)
 	tmp = (*st)->next;
 	(*st)->next = NULL;
 	*st = tmp;
+}
+
+t_list		*atoi_stack(t_list *stack)
+{
+	ssize_t	res;
+	char	sign;
+	char	*s;
+
+	res = 0;
+	s = (char *)stack->cont;
+	sign = (*s == '-' ? -1 : 1);
+	if (*s == '-' || *s == '+')
+		s++;
+	while (*s)
+	{
+		if (*s >= '0' && *s <= '9')
+			res = res * 10 + *s - 48;
+		else
+			return (NULL);
+		s++;
+	}
+	res *= sign;
+	if (res > I_MAX || res < I_MIN)
+		return (NULL);
+	return (ft_lstnew((void *)&res, sizeof(ssize_t)));
+}
+
+void		print_stacks(t_list *a, t_list *b)
+{
+	while (a || b)
+	{
+		printf("%6d %6d\n", (a ? ICONT(a) : 0),
+				(b ? ICONT(b) : 0));
+		if (a)
+			a = a->next;
+		if (b)
+			b = b->next;
+	}
+	printf("%6c %6c\n", '_', '_');
+	printf("%6c %6c\n\n", 'a', 'b');
 }
