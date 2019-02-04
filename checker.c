e7dd0bbf13e2ef6a9b265a1b0c5794da51714010
Merlon crakehall Bartoletti <mbartole@e2r10p5.21-school.ru>
2019-02-04 15:10:04 +0300

    move some useful functions from checker.c to my_stack.s

--- checker.c
+++ checker.c
@@ -6,61 +6,16 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/26 14:24:19 by mbartole          #+#    #+#             */
-/*   Updated: 2019/01/27 02:10:55 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/01/31 16:36:48 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "checker.h"
+#include "swap.h"
 
-#define CONT_SIZE 8
-#define I_MAX 2147483647
-#define I_MIN -2147483648
-
-
-static void	clean(char *msg)//TODO free all
+static int	clean(char *msg)//TODO free all
 {
 	ft_putstr(msg);
-	exit(0);
-}
-
-void		print_stacks(t_list *a, t_list *b)
-{
-	while (a || b)
-	{
-		printf("%6zu %6zu\n", (a ? *((ssize_t *)a->cont) : 0), 
-				(b ? *((ssize_t *)b->cont) : 0));
-		if (a)
-			a = a->next;
-		if (b)
-			b = b->next;
-	}
-	printf("%6c %6c\n", '_', '_');
-	printf("%6c %6c\n\n", 'a', 'b');
-}
-
-t_list		*atoi_stack(t_list *stack)
-{
-	ssize_t	res;
-	char	sign;
-	char	*s;
-
-	res = 0;
-	s = (char *)stack->cont;
-	sign = (*s == '-' ? -1 : 1);
-	if (*s == '-' || *s == '+')
-		s++;
-	while (*s)
-	{
-		if (*s >= '0' && *s <= '9')
-			res = res * 10 + *s - 48;
-		else
-			return (NULL);
-		s++;
-	}
-	res *= sign;
-	if (res > I_MAX || res < I_MIN)
-		return (NULL);
-	return (ft_lstnew((void *)&res, sizeof(ssize_t)));
+	return (0);
 }
 
 static void	check_stack(t_list *st, int count)
@@ -79,12 +34,12 @@ static void	check_stack(t_list *st, int count)
 		if (*((ssize_t *)st->cont) < prev)
 			return ;
 		else if (*((ssize_t *)st->cont) == prev)
-			clean(ERR_M);//TODO
+			exit(clean(ERR_M));//TODO
 		prev = *((ssize_t *)st->cont);
 		st = st->next;
 	}
 	if (count == 0)
-		clean("OK\n");//TODO
+		exit(clean("OK\n"));//TODO
 }
 
 static void	handle_stacks(t_list **a, t_list **b, int count)
@@ -113,11 +68,10 @@ static void	handle_stacks(t_list **a, t_list **b, int count)
 		else if (!ft_strcmp(line, "pb") && (fl = 1))
 			push_stack(a, b);
 		if (!fl)
-			clean(ERR_M);//TODO
+			exit(clean(ERR_M));//TODO
 		free(line);
 		print_stacks(*a, *b);
 		check_stack(*a, count);
-//		printf("waiting instructions\n");
 	}
 }
 
@@ -136,7 +90,7 @@ int			main(int argc, char **argv)
 		ft_lstadd(&a, ft_lstnew(argv[argc], ft_strlen(argv[argc])));
 	b = a;//TODO free b
 	if (!(a = ft_lstmap(b, atoi_stack)))
-		clean(ERR_M);//TODO
+		exit(clean(ERR_M));//TODO
 	//TODO check dublicates 
 	check_stack(a, count);
 	b = NULL;
