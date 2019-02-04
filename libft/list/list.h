17c07fe496d31b0e97e7201a143a3b3c05db9c8b
Merlon crakehall Bartoletti <mbartole@e2r10p5.21-school.ru>
2019-02-04 15:07:52 +0300

    add ft_lstlen.c in libft

--- libft/list/list.h
+++ libft/list/list.h
@@ -6,7 +6,7 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/26 13:51:55 by mbartole          #+#    #+#             */
-/*   Updated: 2019/01/26 14:48:26 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/02/04 07:37:50 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,7 @@ void			ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+
+int				ft_lstlen(t_list *lst);
 
 #endif
