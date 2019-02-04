c3af843a8db990484d6fec49d830d248edbdab59
Merlon crakehall Bartoletti <mbartole@e2r10p5.21-school.ru>
2019-02-04 15:08:49 +0300

    all headers to one - swap.h

--- swap.h
+++ swap.h
@@ -6,7 +6,7 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/26 14:21:02 by mbartole          #+#    #+#             */
-/*   Updated: 2019/01/27 01:10:48 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/02/04 10:25:54 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,13 @@
 
 # define ERR_M "Error\n"
 
+# define ICONT(x) (*((int *)(x)->cont))
+
 void	swap_stack(t_list **st);
 void	push_stack(t_list **src, t_list **dst);
 void	rotate_stack(t_list **st);
 void	r_rotate_stack(t_list **st);
+t_list	*atoi_stack(t_list *stack);
 
 void	print_stacks(t_list *a, t_list *b);
 
