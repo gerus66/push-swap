8c01101045b657db1ac93babb6e990b73cd370c0
Merlon crakehall Bartoletti <mbartole@e4r12p12.21-school.ru>
2019-02-26 05:05:40 +0300

    cosmetic changes in checker

--- checker.c
+++ checker.c
@@ -6,11 +6,57 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/26 14:24:19 by mbartole          #+#    #+#             */
-/*   Updated: 2019/02/11 15:24:36 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/02/26 00:18:35 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "swap.h"
+
+int clean(char *msg)
+{
+	ft_putstr(msg);
+	return (0);
+}
+
+t_list		*atoi_stack(t_list *stack)
+{
+	ssize_t	res;
+	char	sign;
+	char	*s;
+
+	res = 0;
+	s = (char *)stack->cont;
+	sign = (*s == '-' ? -1 : 1);
+	if (*s == '-' || *s == '+')
+		s++;
+	while (*s)
+	{
+		if (*s >= '0' && *s <= '9')
+			res = res * 10 + *s - 48;
+		else
+			return (NULL);
+		s++;
+	}
+	res *= sign;
+	if (res > I_MAX || res < I_MIN)
+		return (NULL);
+	return (ft_lstnew((void *)&res, sizeof(ssize_t)));
+}
+
+void		print_stacks(t_list *a, t_list *b)
+{
+	while (a || b)
+	{
+		printf("%6d %6d\n", (a ? ICONT(a) : 0),
+				(b ? ICONT(b) : 0));
+		if (a)
+			a = a->next;
+		if (b)
+			b = b->next;
+	}
+	printf("%6c %6c\n", '_', '_');
+	printf("%6c %6c\n\n", 'a', 'b');
+}
 
 static void	check_stack(t_list *st, int count)
 {
@@ -35,42 +81,6 @@ static void	check_stack(t_list *st, int count)
 	if (count == 0)
 		exit(clean("OK\n"));//TODO
 }
-
-/*
-void	comm_stacks(t_list **a, t_list **b, t_list *comm)
-{
-	char	*line;
-	int		fl;
-
-	print_stacks(*a, *b);
-	while (comm)
-	{
-		fl = 0;
-		line = (char *)comm->cont;
-		if ((!ft_strcmp(line, "sa") || !ft_strcmp(line, "ss")) && (fl = 1))
-			swap_stack(a);
-		if ((!ft_strcmp(line, "sb") || !ft_strcmp(line, "ss")) && (fl = 1))
-			swap_stack(b);
-		if ((!ft_strcmp(line, "ra") || !ft_strcmp(line, "rr")) && (fl = 1))
-			rotate_stack(a);
-		if ((!ft_strcmp(line, "rb") || !ft_strcmp(line, "rr")) && (fl = 1))
-			rotate_stack(b);
-		if ((!ft_strcmp(line, "rra") || !ft_strcmp(line, "rrr")) && (fl = 1))
-			r_rotate_stack(a);
-		if ((!ft_strcmp(line, "rrb") || !ft_strcmp(line, "rrr")) && (fl = 1))
-			r_rotate_stack(b);
-		if (!ft_strcmp(line, "pa") && (fl = 1))
-			push_stack(b, a);
-		else if (!ft_strcmp(line, "pb") && (fl = 1))
-			push_stack(a, b);
-		if (!fl)
-			exit(clean(ERR_M));//TODO
-	//	free(line);
-		print_stacks(*a, *b);
-		comm = comm->next;
-	//	check_stack(*a, count);
-	}
-}*/
 
 static void	handle_stacks(t_list **a, t_list **b, int count)
 {
