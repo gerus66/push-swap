829b851dae9e76f26b328d8ae0d57b47f9ea2362
Merlon crakehall Bartoletti <mbartole@e4r12p12.21-school.ru>
2019-02-26 05:07:13 +0300

    header

--- swap.h
+++ swap.h
@@ -6,7 +6,7 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/26 14:21:02 by mbartole          #+#    #+#             */
-/*   Updated: 2019/02/12 23:00:26 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/02/26 04:39:40 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,20 +25,47 @@
 # define ICONT(x) (*((int *)(x)->cont))
 # define CCONT(x) ((char *)(x)->cont)
 
+/*
+** operators on stack
+*/
+
 void	swap_stack(t_list **st);
 void	push_stack(t_list **src, t_list **dst);
 void	rotate_stack(t_list **st);
 void	r_rotate_stack(t_list **st);
-t_list	*atoi_stack(t_list *stack);
 
-void	print_stacks(t_list *a, t_list *b);
+/*
+** common for swap and checker functions
+*/
+int		argv_to_list(t_list **in, char **argv, int count);
+void    do_one_comm(t_list **a, t_list **b, t_list *comm);
+void    do_all_comm(t_list **a, t_list **b, t_list *comm);
 
-void    comm_stacks(t_list **a, t_list **b, t_list *comm);
-void    one_comm_stacks(t_list **a, t_list **b, t_list *comm);
+/*
+** utils for push_swap
+*/
 
-void    lst_to_array(t_list *st, int *ar, int count);
-void    tree_to_lst(t_avltree *root, t_list **lst);
-int     atoi_check(char *s);
-int		clean(char *msg);
+//void    lst_to_array(t_list *st, int *ar, int count);
+//void    print_comm(t_list *comm);
+
+/*
+** utils for checker
+*/
+
+//t_list	*atoi_stack(t_list *stack);//TODO
+//void	print_stacks(t_list *a, t_list *b);
+
+/*
+** choose magic sequence
+*/
+
+void        choose_sequence(int *razn, int **standing, int count, char fl);
+
+
+
+
+//void    tree_to_lst(t_avltree *root, t_list **lst);
+//int     atoi_check(char *s);
+//int		clean(char *msg);
 
 #endif
