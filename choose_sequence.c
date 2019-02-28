b5262470c7156d051754b915ca8a3fc87af8b717
Merlon crakehall Bartoletti <mbartole@e1r3p3.21-school.ru>
2019-02-28 14:28:05 +0300

    move choose_sequence process in new file

---
+++ choose_sequence.c
@@ -0,0 +1,155 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   choose_sequence.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/25 22:24:24 by mbartole          #+#    #+#             */
+/*   Updated: 2019/02/28 14:27:16 by mbartole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "swap.h"
+
+static int	*positive_seq(int *razn, int start, int count, char fll)
+{
+	int	*standing;
+	int	i;
+	int	last;
+	int	prev;
+	int	fl;
+
+	if (!(standing = (int *)ft_memalloc(sizeof(int) * count)))
+		return (NULL);
+	standing[start] = 1;
+	last = razn[start];
+	prev = count;//
+	i = start;//
+	fl = 0;
+	while (++i < count)
+	{
+		if (razn[i] <= last && razn[i] >= 0
+				&& razn[i] >= razn[start] - start - (count - i - 1))//
+		{
+			fl++;
+			standing[i] = 1;
+			prev = last;
+			last = razn[i];
+		}
+		else if (razn[i] > last && razn[i] <= prev && fll)
+		{
+			if (fl == 0)
+			{
+				start = i;
+				fl++;
+			}
+			prev = razn[i];
+			standing[i] = -1;
+			last++;//
+		}
+		else
+		{
+			last++;
+			prev++;
+		}
+	}
+	return (standing);
+}
+
+static int	*negative_seq(int *razn, int start, int count, char fll)
+{
+	int	*standing;
+	int	i;
+	int	last;
+	int	prev;
+	int	fl;
+
+	if (!(standing = (int *)ft_memalloc(sizeof(int) * count)))
+		return (NULL);
+	standing[start] = 1;
+	last = razn[start];
+	prev = 0;
+	i = start;//
+	fl = 0;
+	while (++i < count)
+	{
+		if (razn[i] <= last &&
+				razn[i] >= razn[start] - start - (count - i - 1))
+		{
+			fl++;
+			standing[i] = 1;
+			prev = last;
+			last = razn[i];
+		}
+		else if (razn[i] > last && razn[i] <= prev && fll)
+		{
+			if (fl == 0)
+			{
+				start = i;
+				fl++;
+			}
+			prev = razn[i];
+			standing[i] = -1;
+			last++;//
+		}
+		else
+		{
+			last++;
+			prev++;
+		}
+	}
+	return (standing);
+}
+
+static int	stand_count(int *ar, int count)
+{
+	int	stand;
+	int	i;
+
+	stand = 0;
+	i = -1;
+	while (++i < count)
+		if (ar[i])
+			stand++;
+	return (stand);
+}
+
+static void add_array(int *ar, int *add, int count)
+{
+	int	i = -1;
+
+	while (++i < count)
+		if (add[i])
+			ar[i] = 1;
+}
+
+void		choose_sequence(int *razn, int **standing, int count, char fl)
+{
+	int	*standing_tmp;
+	int	i;
+	int stand;
+	int	stand_tmp;
+	int	fill[count];
+
+	i = -1;
+	while (++i < count)
+		fill[i] = 0;
+	*standing = NULL;
+	stand = 0;
+	i = -1;
+	while (++i < count - stand)
+		if (!fill[i])
+		{
+			standing_tmp = (razn[i] <= 0) ? negative_seq(razn, i, count, fl) :
+				positive_seq(razn, i, count, fl);
+			stand_tmp = stand_count(standing_tmp, count);
+			add_array(fill, standing_tmp, count);
+			if (stand_tmp > stand)
+			{
+				free(*standing);
+				*standing = standing_tmp;
+				stand = stand_tmp;
+			}
+		}
+}
