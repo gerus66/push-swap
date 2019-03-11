7f34d0a6762e5b4058f15b60f10d490fc56335c3
Merlon crakehall Bartoletti <mbartole@e3r2p5.21-school.ru>
2019-03-11 19:31:24 +0300

    delete push.c

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/08 12:41:17 by mbartole          #+#    #+#             */
-/*   Updated: 2019/03/01 03:04:30 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/03/11 19:18:57 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,46 @@ int		atoi_check(char *s)
 	if (res > I_MAX || res < I_MIN)
 		exit(clean(ERR_M));//TODO
 	return ((int)res);
+}
+
+t_list		*atoi_stack(t_list *stack)
+{
+	ssize_t	res;
+	char	sign;
+	char	*s;
+
+	res = 0;
+	s = (char *)stack->cont;
+	sign = (*s == '-' ? -1 : 1);
+	if (*s == '-' || *s == '+')
+		s++;
+	while (*s)
+	{
+		if (*s >= '0' && *s <= '9')
+			res = res * 10 + *s - 48;
+		else
+			return (NULL);
+		s++;
+	}
+	res *= sign;
+	if (res > I_MAX || res < I_MIN)
+		return (NULL);
+	return (ft_lstnew((void *)&res, sizeof(ssize_t)));
+}
+
+void		print_stacks(t_list *a, t_list *b)
+{
+	while (a || b)
+	{
+		printf("%6d %6d\n", (a ? ICONT(a) : 0),
+				(b ? ICONT(b) : 0));
+		if (a)
+			a = a->next;
+		if (b)
+			b = b->next;
+	}
+	printf("%6c %6c\n", '_', '_');
+	printf("%6c %6c\n\n", 'a', 'b');
 }
 
 void	do_one_comm(t_list **a, t_list **b, t_list *comm)
