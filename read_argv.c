b448b760f9dd7b81540ec6a7c4870fd987948e62
Merlon crakehall Bartoletti <gerus66@gmail.com>
2019-04-11 01:26:08 +0300

    read_argv.c Norme. used by both binary, check validity, return list from argvs

--- read_argv.c
+++ read_argv.c
@@ -6,13 +6,13 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 04:26:20 by mbartole          #+#    #+#             */
-/*   Updated: 2019/04/10 04:41:24 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/04/11 01:17:59 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "swap.h"
 
-static int		atoi_check(char *s)
+static int		atoi_check(char *s, t_list *lst)
 {
 	ssize_t	res;
 	char	sign;
@@ -26,23 +26,36 @@ static int		atoi_check(char *s)
 		if (*s >= '0' && *s <= '9')
 			res = res * 10 + *s - 48;
 		else
-			exit(clean(ERR_M));//TODO
+			exit(clean(ERR_M, &lst, 1));
 		s++;
 	}
 	res *= sign;
 	if (res > I_MAX || res < I_MIN)
-		exit(clean(ERR_M));//TODO
+		exit(clean(ERR_M, &lst, 1));
 	return ((int)res);
 }
 
-static void	simplify(t_list *in)
+static void		double_check(int val, t_list *in)
+{
+	t_list	*cp;
+
+	cp = in;
+	while (cp)
+	{
+		if (ICONT(cp) == val)
+			exit(clean(ERR_M, &in, 1));
+		cp = cp->next;
+	}
+}
+
+static void		simplify(t_list *in)
 {
 	int	*sorted;
 	int	i;
 	int	count;
 
 	count = ft_lstlen(in);
-	sorted = get_diff(in, 0, 0, ft_lstlen(in));
+	sorted = get_diff(in, 0, 0, count);
 	while (in)
 	{
 		i = -1;
@@ -54,9 +67,10 @@ static void	simplify(t_list *in)
 			}
 		in = in->next;
 	}
+	free(sorted);
 }
 
-int	argv_to_list(t_list **in, char **argv, int count, char simple)
+int				argv_to_list(t_list **in, char **argv, int count, char simple)
 {
 	int		i;
 	char	**ar;
@@ -64,18 +78,18 @@ int	argv_to_list(t_list **in, char **argv, int count, char simple)
 	int		tmp;
 	int		len;
 
-	i = 0;
 	len = 0;
+	i = 0;
 	while (++i <= count)
 	{
 		ar = ft_strsplit(argv[i], ' ');
 		cp = ar;
-		while (*ar)
+		while (*ar && ++len)
 		{
-			tmp = atoi_check(*ar);
+			tmp = atoi_check(*ar, *in);
 			free(*ar);
+			double_check(tmp, *in);
 			ft_lstadd_back(in, ft_lstnew((void *)&tmp, sizeof(int)));
-			len++;
 			ar++;
 		}
 		free(cp);
