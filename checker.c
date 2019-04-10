b9391e6a48333573ba9a5a314c253b98845b7d21
Merlon crakehall Bartoletti <gerus66@gmail.com>
2019-04-11 02:32:50 +0300

    checker.c Norme, pass full tests

--- checker.c
+++ checker.c
@@ -6,20 +6,20 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/26 14:24:19 by mbartole          #+#    #+#             */
-/*   Updated: 2019/04/10 05:17:45 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/04/11 02:31:45 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "swap.h"
 
-static void	check_stack(t_list *st, int count)
+static void	check_stacks(t_list *st, int count, t_list *ya_st)
 {
-	int	fl;
 	int	prev;
 
-	if (!st || !st->next)
+	if (!st)
 		return ;
-	fl = 1;
+	if (!st->next && count == 1)
+		exit(clean(OK_M, (t_list *[]){st, ya_st}, 2));
 	prev = *((int *)st->cont);
 	st = st->next;
 	count--;
@@ -28,46 +28,58 @@ static void	check_stack(t_list *st, int count)
 		if (*((int *)st->cont) < prev)
 			return ;
 		else if (*((int *)st->cont) == prev)
-			exit(clean(ERR_M));//TODO
+			exit(clean(KO_M, (t_list *[]){st, ya_st}, 2));
 		prev = *((int *)st->cont);
 		st = st->next;
 	}
 	if (count == 0)
-		exit(clean("OK\n"));//TODO
+		exit(clean(OK_M, (t_list *[]){st, ya_st}, 2));
+}
+
+static char	switch_ops(t_list **a, t_list **b, char *line)
+{
+	char	fl;
+
+	fl = 0;
+	if ((!ft_strcmp(line, "sa") || !ft_strcmp(line, "ss")) && (fl = 1))
+		swap_stack(a);
+	if ((!ft_strcmp(line, "sb") || !ft_strcmp(line, "ss")) && (fl = 1))
+		swap_stack(b);
+	if ((!ft_strcmp(line, "ra") || !ft_strcmp(line, "rr")) && (fl = 1))
+		rotate_stack(a);
+	if ((!ft_strcmp(line, "rb") || !ft_strcmp(line, "rr")) && (fl = 1))
+		rotate_stack(b);
+	if ((!ft_strcmp(line, "rra") || !ft_strcmp(line, "rrr")) && (fl = 1))
+		r_rotate_stack(a);
+	if ((!ft_strcmp(line, "rrb") || !ft_strcmp(line, "rrr")) && (fl = 1))
+		r_rotate_stack(b);
+	if (!ft_strcmp(line, "pa") && (fl = 1))
+		push_stack(b, a);
+	else if (!ft_strcmp(line, "pb") && (fl = 1))
+		push_stack(a, b);
+	return (fl);
 }
 
 static void	handle_stacks(t_list **a, t_list **b, int count)
 {
 	char	*line;
-	int		fl;
 
-	print_stacks(*a, *b);
 	while (get_next_line(0, &line))
 	{
-		fl = 0;
-		if ((!ft_strcmp(line, "sa") || !ft_strcmp(line, "ss")) && (fl = 1))
-			swap_stack(a);
-		if ((!ft_strcmp(line, "sb") || !ft_strcmp(line, "ss")) && (fl = 1))
-			swap_stack(b);
-		if ((!ft_strcmp(line, "ra") || !ft_strcmp(line, "rr")) && (fl = 1))
-			rotate_stack(a);
-		if ((!ft_strcmp(line, "rb") || !ft_strcmp(line, "rr")) && (fl = 1))
-			rotate_stack(b);
-		if ((!ft_strcmp(line, "rra") || !ft_strcmp(line, "rrr")) && (fl = 1))
-			r_rotate_stack(a);
-		if ((!ft_strcmp(line, "rrb") || !ft_strcmp(line, "rrr")) && (fl = 1))
-			r_rotate_stack(b);
-		if (!ft_strcmp(line, "pa") && (fl = 1))
-			push_stack(b, a);
-		else if (!ft_strcmp(line, "pb") && (fl = 1))
-			push_stack(a, b);
-		if (!fl)
-			exit(clean(ERR_M));//TODO
+		if (!ft_strcmp(line, ""))
+		{
+			free(line);
+			return ;
+		}
+		if (!switch_ops(a, b, line))
+		{
+			free(line);
+			exit(clean(ERR_M, (t_list *[]){*a, *b}, 2));
+		}
 		free(line);
 		print_stacks(*a, *b);
-		check_stack(*a, count);
+		check_stacks(*a, count, *b);
 	}
-	exit(clean(ERR_M));
 }
 
 int			main(int argc, char **argv)
@@ -76,14 +88,14 @@ int			main(int argc, char **argv)
 	t_list	*b;
 	int		count;
 
-	if (argc == 2)
-		write(1, "OK\n", 3);
-	if (argc < 3)
+	if (argc < 2)
 		return (0);
-	count = argv_to_list(&a, argv, argc - 1, 0);
-	//TODO check dublicates 
-	check_stack(a, count);
+	a = NULL;
 	b = NULL;
+	count = argv_to_list(&a, argv, argc - 1, 0);
+	print_stacks(a, b);
 	handle_stacks(&a, &b, count);
+	check_stacks(a, count, b);
+	clean(KO_M, (t_list *[]){a, b}, 2);
 	return (0);
 }
