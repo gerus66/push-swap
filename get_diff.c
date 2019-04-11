07fc14878346c0ef6b457246a18ba04bd8278bee
Merlon crakehall Bartoletti <gerus66@gmail.com>
2019-04-11 03:58:48 +0300

    get_diff.c Norme; return sorted array or array of diffs; think about free tree

--- get_diff.c
+++ get_diff.c
@@ -6,22 +6,23 @@
 /*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/25 20:35:30 by mbartole          #+#    #+#             */
-/*   Updated: 2019/04/09 18:59:28 by mbartole         ###   ########.fr       */
+/*   Updated: 2019/04/11 03:57:50 by mbartole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "swap.h"
 
-static void	tree_to_lst(t_avltree *root, t_list **lst)
+static void		tree_to_lst(t_avltree *root, t_list **lst)
 {
 	if (root == NULL)
 		return ;
 	tree_to_lst(root->left, lst);
 	ft_lstadd_back(lst, ft_lstnew((void *)&root->key, sizeof(int)));
 	tree_to_lst(root->right, lst);
+	free(root);
 }
 
-static void	lst_to_array(t_list *st, int *ar, int count)
+static void		lst_to_array(t_list *st, int *ar, int count)
 {
 	int	i;
 
@@ -33,76 +34,69 @@ static void	lst_to_array(t_list *st, int *ar, int count)
 	}
 }
 
-static int	*init_razn(int *sorted, t_list *in, int count)
+static void		reverse_it(int *sorted_ar, int len)
+{
+	int	i;
+	int	tmp;
+
+	i = -1;
+	while (++i < len / 2)
+	{
+		tmp = sorted_ar[i];
+		sorted_ar[i] = sorted_ar[len - i - 1];
+		sorted_ar[len - i - 1] = tmp;
+	}
+}
+
+static void		init_razn(int *sorted, t_list *in, int count)
 {
 	int	i;
 	int	j;
-	int	k;
 	int	tmp;
 	int	*razn;
 
-
 	razn = (int *)ft_memalloc(sizeof(int) * count);
-	j = 0;
-	k = -1;
-	while (in && ++k < count)
+	j = -1;
+	while (in)
 	{
 		i = -1;
 		while (++i < count)
 			if (ICONT(in) == sorted[i])
 				break ;
-		tmp = j - i;
+		tmp = ++j - i;
 		if (tmp < -count / 2)
 			tmp += count;
 		else if (tmp > count / 2)
 			tmp -= count;
 		razn[j] = tmp;
 		in = in->next;
-		j++;
 	}
-	free(sorted);
-//	print_seq(razn, count);//
-	return (razn);
+	i = -1;
+	while (++i < count)
+		sorted[i] = razn[i];
+	free(razn);
 }
 
-int		*get_diff(t_list *in, char fl, char reverse, int number)
+void			get_diff(t_list *in, int *sorted_ar, char razn, char reverse)
 {
-	t_avltree *tr;
-	t_list	*cp;
-	int		*sorted_ar;
-	int		len;
-	int		i;
+	t_avltree	*tr;
+	t_list		*cp;
+	int			len;
 
-	len = MIN(ft_lstlen(in), number);
 	tr = NULL;
 	cp = in;
-	i = -1;
-	while (cp && ++i < len)	
+	while (cp)
 	{
 		ft_tree_insert(&tr, ICONT(cp), NULL, 0);
 		cp = cp->next;
 	}
 	cp = NULL;
 	tree_to_lst(tr, &cp);
-	//TODO free tree
-	sorted_ar = (int *)malloc(sizeof(int) * len);
+	len = ft_lstlen(in);
 	lst_to_array(cp, sorted_ar, len);
 	ft_lstdel(&cp, NULL);
-//	print_seq(sorted_ar, len);
 	if (reverse)
-	{
-		i = -1;
-		int	tmp;
-		while (++i < len / 2)
-		{
-			tmp = sorted_ar[i];
-			sorted_ar[i] = sorted_ar[len - i - 1];
-			sorted_ar[len - i - 1] = tmp;
-		}
-	}
-//	print_seq(sorted_ar, len);
-	if (fl)
-		return (init_razn(sorted_ar, in, len));
-	else
-		return(sorted_ar);
+		reverse_it(sorted_ar, len);
+	if (razn)
+		init_razn(sorted_ar, in, len);
 }
