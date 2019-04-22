d04bc1dac40820e26c9324cf5cf9f68dba9dacb9
Merlon crakehall Bartoletti <gerus66@gmail.com>
2019-04-22 11:53:29 +0300

    generator

---
+++ generator.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   generator.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbartole <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/07 15:04:16 by mbartole          #+#    #+#             */
+/*   Updated: 2019/04/19 20:35:17 by mbartole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char **argv)
+{
+	int	tmp;
+	int	j;
+
+	if (argc != 2)
+		return (0);
+	int count = atoi(argv[1]);
+	int *ar = (int *)malloc(sizeof(int) * count);
+	srand(time(NULL));
+	int	i = 0;
+	while (i < count)
+	{
+		tmp = rand() % 100000;
+		j = -1;
+		while (++j < i)
+			if (tmp == ar[j])
+				break ;
+		if (j == i)
+		{
+			ar[i] = tmp;
+			printf("%d ", tmp);
+			i++;
+		}
+	}
+	return (0);
+}
